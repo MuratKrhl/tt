493d630d05d61a9cb0a7f58a991faf44f991b15d
MuratKrhl <113339316+MuratKrhl@users.noreply.github.com>
2025-08-20 18:15:37 +0300

    Update models.py

--- Yeni klasör (4)/user_management/models.py
+++ Yeni klasör (4)/user_management/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+#from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
